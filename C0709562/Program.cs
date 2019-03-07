ed8f9d391c72e9400609de05deb28bf96b0c77b4
visualstudio <visualstudio@230-PC14>
2019-03-07 15:45:17 -0500

    C0709562__Lab1

--- C0709562/Program.cs
+++ C0709562/Program.cs
@@ -4,12 +4,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C0709562
+namespace ConsoleApp1
+
 {
+
     class Program
+
     {
+
         static void Main(string[] args)
+
         {
+
+            //Student name Sukhraj Singh Student ID C0709562
+
+            //CSD3354 Section2
+
+            //Assignment 2
+
+            //March 7,2019
+
         }
+
     }
+
+}
+
+
+
+public class DelegateExercises
+
+{
+
+    public delegate void MyDelegate();
+
+
+
+    void Method1()
+
+    {
+
+        Console.WriteLine("Method1");
+
+        Console.ReadLine();
+
+
+
+    }
+
+
+
+    public void Method2()
+
+    {
+
+        MyDelegate myDelegate = new MyDelegate(Method1);
+
+        myDelegate();
+
+
+
+    }
+
+}
+
+
+
+namespace DelegatesAndEvents
+
+{
+
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            // TO DO: Call Method2 from the DelegateExercises Class
+
+        }
+
+    }
+
 }
